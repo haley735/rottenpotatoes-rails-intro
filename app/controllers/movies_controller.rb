c0d7f81869ccada3f478e4afe90c3178c8ddb14c
EC2 Default User <ec2-user@ip-172-31-10-245.us-east-2.compute.internal>
2019-10-01 15:58:34 +0000

    part 3 complete?

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,24 +6,50 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-
   def index
-    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    @checked_ratings = check
-    @checked_ratings.each do |rating|
-      params[rating] = true
+    # Deal with movie ratings now...
+    @all_ratings = Movie.get_possible_ratings 
+
+    unless params[:ratings].nil?
+      @filtered_ratings = params[:ratings]
+      session[:filtered_ratings] = @filtered_ratings
     end
 
-    if params[:sort]
-      @movies = Movie.order(params[:sort])
+    if params[:sorting_mechanism].nil?
+      # If user didn't specify a sorting mechanism, then we're going to sort by the
+      # sorting mechanism in our sessions
     else
-      @movies = Movie.where(:rating => @checked_ratings)
+      session[:sorting_mechanism] = params[:sorting_mechanism]
+    end
+
+    if params[:sorting_mechanism].nil? && params[:ratings].nil? && session[:filtered_ratings]
+      @filtered_ratings = session[:filtered_ratings]
+      @sorting_mechanism = session[:sorting_mechanism]
+      flash.keep
+      redirect_to movies_path({order_by: @sorting_mechanism, ratings: @filtered_ratings})
+    end
+
+    @movies = Movie.all
+
+    if session[:filtered_ratings]
+      @movies = @movies.select{ |movie| session[:filtered_ratings].include? movie.rating }
+    end
+
+    if session[:sorting_mechanism] == "title"
+      @movies = @movies.sort! { |a,b| a.title <=> b.title }
+      @movie_highlight = "hilite"
+    elsif session[:sorting_mechanism] == "release_date"
+      @movies = @movies.sort! { |a,b| a.release_date <=> b.release_date }
+      @date_highlight = "hilite"
+    else
+      
     end
   end
 
   def new
     # default: render 'new' template
   end
+
 
   def create
     @movie = Movie.create!(params[:movie])
@@ -49,13 +75,14 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  private
-
-  def check
-    if params[:ratings]
-      params[:ratings].keys
+def find_with_same_director
+    @movie = Movie.find(params[:id])
+    @current_movie_director = @movie.director
+    if @current_movie_director.nil? or @current_movie_director.empty?
+flash[:warning] = "'#{@movie.title}' has no director info."
+      redirect_to movies_path
     else
-      @all_ratings
+@movies_to_render = Movie.find_all_by_director(@current_movie_director)
     end
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ActiveRecord::Base
     def self.all_ratings
 	self.fine(:all, :select => "rating", :group => "rating").map(&:rating)
     end
+
+  attr_accessible :title, :rating, :description, :release_date, :director
+
+  def self.get_possible_ratings
+	return %w[G PG PG-13 R]
+  end
+
+
 end
