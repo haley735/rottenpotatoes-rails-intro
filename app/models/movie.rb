981593059d2941072b247903b3f146a00c135efe
EC2 Default User <ec2-user@ip-172-31-10-245.us-east-2.compute.internal>
2019-10-01 16:25:54 +0000

    fixing typo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
 	self.fine(:all, :select => "rating", :group => "rating").map(&:rating)
     end
 
-  attr_accessible :title, :rating, :description, :release_date, :director
+  attr_accessor :title, :rating, :description, :release_date, :director
 
   def self.get_possible_ratings
 	return %w[G PG PG-13 R]
